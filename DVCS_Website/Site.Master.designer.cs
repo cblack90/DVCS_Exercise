b1422c2d1e9f3dc4cd2c19b575a13492144b21a4
cblack90 <46687746+cblack90@users.noreply.github.com>
2019-09-15 18:26:29 -0600

    Added a web.sitemap file and created a simple drop down menu at the top of the page to show demo pages

--- DVCS_Website/Site.Master.designer.cs
+++ DVCS_Website/Site.Master.designer.cs
@@ -13,6 +13,24 @@ namespace DVCS_Website {
     public partial class SiteMaster {
         
         /// <summary>
+        /// BasicASPXMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu BasicASPXMenu;
+        
+        /// <summary>
+        /// SiteNav control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteNav;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
